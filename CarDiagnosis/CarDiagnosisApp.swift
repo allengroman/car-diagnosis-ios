8459fc10fffbf76ff4b1e5db14f8c548b654bd2d
Allen <allengroman10@gmail.com>
2024-04-15 12:27:57 -0400

    Initial Commit

---
+++ CarDiagnosis/CarDiagnosisApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarDiagnosisApp.swift
+//  CarDiagnosis
+//
+//  Created by Allen Roman on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct CarDiagnosisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
